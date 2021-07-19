ff610db3b2b4b76bc8395d20b46d1a3fb457f4e9
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-18 22:21:47 -0500

    adding the first states and synthetic events

--- react-form/src/components/Form.jsx
+++ react-form/src/components/Form.jsx
@@ -6,21 +6,27 @@ export default function Form() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    let newUser = {userName,email,password};
+    console.log("Hello and welcome",newUser); 
+  } 
+
   return (
-    <form>
+    <form onSubmit={onSubmit} >
       <h1>This is a react form</h1>
       <label htmlFor="user">User Name: </label>
-      <input type="text" id="user" />
+      <input type="text" id="user" value={userName} onChange={e=>setUser(e.target.value)}/>
       <br/>
 
       <label htmlFor="email">You Email: </label>
-      <input type="email" id="email" />
+      <input type="email" id="email" value={email} onChange={e=>setEmail(e.target.value)} />
       <br/>
 
       <label htmlFor="password">Password: </label>
-      <input type="password" id="password" />
+      <input type="password" id="password" value={password} onChange={e=>setPassword(e.target.value)} />
       <br/>
-      <input type="submit" value="Login" />
+      <input type="submit" value="Create User" />
     </form>
   )
 }
