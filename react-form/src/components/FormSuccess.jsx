3aeae7396196e7fde11d16c2a4c8fa8dec356abb
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-20 15:18:13 -0500

    finished form success form styles

--- react-form/src/components/FormSuccess.jsx
+++ react-form/src/components/FormSuccess.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FormImg2 = styled.img`
+  width: 60%;
+  height: 60%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const FormRight = styled.div`
+  border-radius: 0 10px 10px 0;
+  position: relative;
+  background: linear-gradient(90deg, rgb(40, 40, 40) 0%, rgb(17, 17, 17) 100%);
+
+`
+
+const Success = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  margin-top: 80px;
+  color: #fff;
+`
 
 
 export default function FormSuccess() {
   return (
-    <div className="form-content-right">
-      <div className="form-success">
-        <h1 className='form-success'>We have received your request!</h1>
-        <img src="img/img-3.svg" alt="succes-image"
-        className="form-img-2" />
-      </div>
-      
-    </div>
+    <FormRight>
+      <Success>We have received your request!</Success>
+      <FormImg2 src="img/img-3.svg" alt="succes-image"/> 
+    </FormRight>
   );
 }
